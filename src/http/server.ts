6491bca1df1c352b9575c720e76a66601745b813
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-02-14 18:04:47 -0500

    Remove node modules

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,5 +3,5 @@ import fastify from "fastify";
 const app = fastify();
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("🚀HTTP server listening on port 3333!🚀");
+  console.log("🚀 HTTP server listening on port 3333! 🚀");
 });
