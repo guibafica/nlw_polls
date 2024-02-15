01b9f623c721ab40941550365d4c66016f3f1a65
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-02-15 16:16:56 -0500

    Configs docker compose

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,10 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.get("/", () => {
+  return "Hello world!";
+});
+
 app.listen({ port: 3333 }).then(() => {
   console.log("🚀HTTP server listening on port 3333!🚀");
 });
